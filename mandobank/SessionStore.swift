80b6e7f1dd09a5ee4bacb96227c68fab3ade5c24
Zeeshan Hooda <zeeshanhooda@icloud.com>
2019-12-16 17:36:31 -0700

    Initial Cocoa Pod Setup

---
+++ mandobank/SessionStore.swift
@@ -0,0 +1,58 @@
+//
+//  SessionStore.swift
+//  mandobank
+//
+//  Created by Zeeshan Hooda on 12/16/19.
+//  Copyright © 2019 Deceptive Labs. All rights reserved.
+//
+
+import SwiftUI
+import Firebase
+import Combine
+
+class SessionStore : ObservableObject {
+    var didChange = PassthroughSubject<SessionStore, Never>()
+    var session: User? { didSet { self.didChange.send(self) }}
+    var handle: AuthStateDidChangeListenerHandle?
+
+    func listen () {
+        // monitor authentication changes using firebase
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            if let user = user {
+                // if we have a user, create a new user model
+                print("Got user: \(user)")
+                self.session = User(
+                    uid: user.uid,
+                    displayName: user.displayName,
+                    email: user.email
+                )
+            } else {
+                // if we don't have a user, set our session to nil
+                self.session = nil
+            }
+        }
+    }
+
+    // Sign up function
+    func signUp(
+        email: String,
+        password: String,
+        handler: @escaping AuthDataResultCallback
+        ) {
+        Auth.auth().createUser(withEmail: <#T##String#>, password: <#T##String#>, completion: <#T##AuthDataResultCallback?##AuthDataResultCallback?##(AuthDataResult?, Error?) -> Void#>)
+    }
+    
+    
+}
+
+struct User {
+    var uid: String
+    var email: String?
+    var displayName: String?
+    
+    init(uid: String, displayName: String?, email: String?) {
+        self.uid = uid
+        self.email = email
+        self.displayName = displayName
+    }
+}
